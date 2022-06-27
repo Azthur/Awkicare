0354d626a1e7c66d2ba93c095f8fc30029bd2588
Richard D <rdro2005@hotmail.com>
2022-06-26 22:44:41 -0500

    Se agrego la clase Pagos

--- build/generated/src/org/apache/jsp/index_jsp.java
+++ build/generated/src/org/apache/jsp/index_jsp.java
@@ -41,21 +41,21 @@ public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
       _jspx_out = out;
       _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
 
-      out.write("\n");
-      out.write("\n");
-      out.write("<!DOCTYPE html>\n");
-      out.write("<html>\n");
-      out.write("    <head>\n");
-      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
-      out.write("        <title>JSP Page</title>\n");
-      out.write("    </head>\n");
-      out.write("    <body>\n");
-      out.write("        <h1>Mantenimiento de Enfermeros</h1>\n");
-      out.write("        <a href=\"ControlEnfermeros?accion=agregarEnfermero\">Ingresar enfermero</a><br>\n");
-      out.write("        <a href=\"ControlEnfermeros?accion=listarEnfermeros\">Listar enfermeros</a><br>\n");
-      out.write("        \n");
-      out.write("    </body>\n");
-      out.write("</html>\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html>\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("        <title>JSP Page</title>\r\n");
+      out.write("    </head>\r\n");
+      out.write("    <body>\r\n");
+      out.write("        <h1>Mantenimiento de Enfermeros</h1>\r\n");
+      out.write("        <a href=\"ControlEnfermeros?accion=agregarEnfermero\">Ingresar enfermero</a><br>\r\n");
+      out.write("        <a href=\"ControlEnfermeros?accion=listarEnfermeros\">Listar enfermeros</a><br>\r\n");
+      out.write("        \r\n");
+      out.write("    </body>\r\n");
+      out.write("</html>\r\n");
     } catch (Throwable t) {
       if (!(t instanceof SkipPageException)){
         out = _jspx_out;
