d0194f1982740606a832a16d615c49be9edf7e16
Richard D <rdro2005@hotmail.com>
2022-06-29 21:23:57 -0500

    Se agregaron los DAOs Pagos y Solicitudes_atendidas y se implementaron sus metodos abtractos, pendiente de agregar los camposa dichos metods abtractos

---
+++ src/java/modeloDAO/Solicitudes_atendidasDAO.java
@@ -0,0 +1,16 @@
+package modeloDAO;
+
+import config.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import misInterfaces.Solicitudes_atendidasInterface;
+import modelo.Solicitudes_atendidas;
+
+public class Solicitudes_atendidasDAO {
+    
+}
