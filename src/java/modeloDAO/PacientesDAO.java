0ae325a477013de60c17acd12f899a26ec0fd1b5
Richard D <rdro2005@hotmail.com>
2022-06-30 01:12:53 -0500

    Se completo el DAO pacientes

--- src/java/modeloDAO/PacientesDAO.java
+++ src/java/modeloDAO/PacientesDAO.java
@@ -21,27 +21,113 @@ public class PacientesDAO implements PacientesInterface{
 
     @Override
     public boolean agregar(Pacientes pa) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String sql = "insert into pacientes values(null,'1',?,?,?,?,?,?,?,?,?,'1')";
+            con = cn.getConexion();
+            ps = con.prepareStatement(sql);
+            
+            ps.setString(1, pa.getNombres());
+            ps.setString(2, pa.getApellidos());
+            ps.setInt(3, pa.getDni());
+            ps.setString(4, pa.getGenero());
+            ps.setString(5, pa.getDireccion());
+            ps.setInt(6, pa.getUbigeo());
+            ps.setString(7, pa.getCorreo());
+            ps.setString(8, pa.getTelefono());
+            ps.setString(9, pa.getNacimiento());
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PacientesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return false;
     }
 
     @Override
     public boolean eliminar(int idpacientes) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       try {
+            String sql = "delete from pacientes where idpacientes="+idpacientes;
+            con = cn.getConexion();
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PacientesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return false;
     }
 
     @Override
     public boolean editar(Pacientes pa) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String sql = "update pacientes set direccion=?, ubigeo=?, correo=?, telefono=? where idpacientes="+pa.getIdpacientes();
+            con = cn.getConexion();
+            ps = con.prepareStatement(sql);
+            ps.setString(1, pa.getDireccion());
+            ps.setInt(2, pa.getUbigeo());
+            ps.setString(3, pa.getCorreo());
+            ps.setString(4, pa.getTelefono());
+            
+            ps.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(PacientesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return false;
     }
 
     @Override
     public ArrayList<Pacientes> listarTodos() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+       try {
+            String sql = "select * from pacientes";
+            con = cn.getConexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                pa = new Pacientes();
+                pa.setIdpacientes(rs.getInt("idpacientes"));
+                pa.setIdusuarios(rs.getInt("idusuarios"));
+                pa.setNombres(rs.getString("nombres"));
+                pa.setApellidos(rs.getString("apellidos"));
+                pa.setDni(rs.getInt("dni"));
+                pa.setGenero(rs.getString("genero"));
+                pa.setDireccion(rs.getString("direccion"));
+                pa.setUbigeo(rs.getInt("ubigeo"));
+                pa.setCorreo(rs.getString("correo"));
+                pa.setTelefono(rs.getString("telefono"));
+                pa.setNacimiento(rs.getString("nacimiento"));
+                pa.setEstado(rs.getInt("estado"));
+                lista.add(pa);              
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PacientesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return lista;
     }
 
     @Override
     public Pacientes listarUno(int idpacientes) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        try {
+            String sql = "select * from pacientes where idpacientes="+idpacientes;
+            con = cn.getConexion();
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                pa = new Pacientes();
+                pa.setIdpacientes(rs.getInt("idpacientes"));
+                pa.setIdusuarios(rs.getInt("idusuarios"));
+                pa.setNombres(rs.getString("nombres"));
+                pa.setApellidos(rs.getString("apellidos"));
+                pa.setDni(rs.getInt("dni"));
+                pa.setGenero(rs.getString("genero"));
+                pa.setDireccion(rs.getString("direccion"));
+                pa.setUbigeo(rs.getInt("ubigeo"));
+                pa.setCorreo(rs.getString("correo"));
+                pa.setTelefono(rs.getString("telefono"));
+                pa.setNacimiento(rs.getString("nacimiento"));
+                pa.setEstado(rs.getInt("estado"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(PacientesDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }        
+        return pa;
     }
         
 }
