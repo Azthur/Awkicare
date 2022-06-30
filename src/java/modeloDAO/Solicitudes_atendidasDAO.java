9e6b1a34ea15864224cc1b075c52e5ccf6b14baf
Richard D <rdro2005@hotmail.com>
2022-06-29 21:26:15 -0500

    cargando actualizaciones (falto control + S)

--- src/java/modeloDAO/Solicitudes_atendidasDAO.java
+++ src/java/modeloDAO/Solicitudes_atendidasDAO.java
@@ -11,6 +11,37 @@ import java.util.logging.Logger;
 import misInterfaces.Solicitudes_atendidasInterface;
 import modelo.Solicitudes_atendidas;
 
-public class Solicitudes_atendidasDAO {
+public class Solicitudes_atendidasDAO implements Solicitudes_atendidasInterface{
+    Conexion cn = new Conexion();
+    Connection con;
+    PreparedStatement ps;
+    ResultSet rs;
+    Solicitudes_atendidas sa;
+    ArrayList<Solicitudes_atendidas>lista = new ArrayList<>();
+
+    @Override
+    public boolean agregar(Solicitudes_atendidas e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean eliminar(String idsolicitudes_atendidas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean editar(Solicitudes_atendidas e) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ArrayList<Solicitudes_atendidas> listarTodos() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Solicitudes_atendidas listarUno(int idsolicitudes_atendidas) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
